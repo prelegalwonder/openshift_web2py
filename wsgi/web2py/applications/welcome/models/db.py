4bd1b9bda860c3dc2b741e639b7bc1182a5848c7
Andrew Replogle <areplogle@ARThinkPad.(none)>
2012-05-24 14:49:22 -0500

    testing folder= in DAL declaration

--- wsgi/web2py/applications/welcome/models/db.py
+++ wsgi/web2py/applications/welcome/models/db.py
@@ -8,6 +8,7 @@
 ## if SSL/HTTPS is properly configured and you want all HTTP requests to
 ## be redirected to HTTPS, uncomment the line below:
 # request.requires_https()
+import os
 SQLITE_DIR = os.path.join(os.environ['OPENSHIFT_DATA_DIR'],'databases')
 
 if not request.env.web2py_runtime_gae:
